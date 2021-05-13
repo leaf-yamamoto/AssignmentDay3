8649eb2bc22458938d8a20a80ccb87d5421241e0
leaf-yamamoto <sanaeyamamoto@icloud.com>
2021-05-13 17:27:05 +0900

    サウンドアプリ2完成

--- app/src/main/java/app/yamamoto/leaf/sound/MainActivity.kt
+++ app/src/main/java/app/yamamoto/leaf/sound/MainActivity.kt
@@ -3,6 +3,7 @@ package app.yamamoto.leaf.sound
 import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,9 +13,20 @@ class MainActivity : AppCompatActivity() {
 
         val drumSound = MediaPlayer.create(this, R.raw.drum_sound)
 
-        drumImage.setOnClickListener{
-            drumSound.seekTo(0)
-            drumSound.start()
+        drumImage.setOnTouchListener { view, event ->
+
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                drumImage.setImageResource(R.drawable.drum_playing_image)
+
+                drumSound.seekTo(0)
+                drumSound.start()
+
+            } else if (event.action == MotionEvent.ACTION_UP) {
+                drumImage.setImageResource(R.drawable.drum_image)
+            }
+
+                true
+            }
         }
     }
-}
+
